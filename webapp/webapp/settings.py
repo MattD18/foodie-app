39e2984a1cc9a3d67b141f9d3b2534bb816faeb4
Matthew Dalton <mgd67@cornell.edu>
2023-09-10 11:55:22 -0400

    setting up chatapp

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chatapp',
     'foodie.apps.FoodieConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -129,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__),'static'),)
